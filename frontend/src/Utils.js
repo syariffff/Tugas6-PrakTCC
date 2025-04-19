a56942179261da4d4031a8d9b4f177d580f15b75
Syarif <121801454+syariffff@users.noreply.github.com>
2025-04-19 21:41:19 +0700

    Update Utils.js

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://notes-backend-syarif-995193249744.us-central1.run.app/notes";
+export const BASE_URL = "https://notes-backend-syarif-995193249744.us-central1.run.app";
